77d2c660897b68c12b65b35ffe73f46d50153778
hirdle <ilyapower368@gmail.com>
2023-05-04 15:09:49 +0300

    add requirements

--- app/settings.py
+++ app/settings.py
@@ -141,6 +141,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = '/home/c74371/gpt-generator.na4u.ru/www/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
