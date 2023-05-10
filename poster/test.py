e53a08fa3e75930d2381e609bedf05e98f348aa5
hirdle <ilyapower368@gmail.com>
2023-05-10 16:53:12 +0300

    new

--- poster/test.py
+++ poster/test.py
@@ -1,4 +1,4 @@
 j = {"telegram_id": -1001856652818, "owner": 1}
 import requests
-r = requests.put(f'http://gpt-tool.ru/api/channels/2/', data=j)
+r = requests.put(f'http://gpt-tool.ru/api/channels/2/update/', data=j)
 print(r.json())
