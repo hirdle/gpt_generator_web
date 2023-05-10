f866480b8593e1ad893fdcb423955a48e53854aa
hirdle <ilyapower368@gmail.com>
2023-05-10 17:00:11 +0300

    new

--- users/models.py
+++ users/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 class Channel(models.Model):
 
     name = models.CharField(max_length=255, default="Default")
-    telegram_id = models.IntegerField()
+    telegram_id = models.IntegerField(null=True, blank=True)
     themes = models.TextField(default="", null=True, blank=True)
     template = models.TextField(default="*Theme*")
     publish_interval = models.TextField(default="")
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
 
 
